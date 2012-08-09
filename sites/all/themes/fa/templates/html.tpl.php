2d295d5ab623797351812a467e001e6493122592
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2012-08-09 09:35:42 +0300

    Added layout for login and blog release date

--- sites/all/themes/fa/templates/html.tpl.php
+++ sites/all/themes/fa/templates/html.tpl.php
@@ -82,7 +82,9 @@
   <?php if ($default_mobile_metatags): ?>
     <meta name="MobileOptimized" content="width">
     <meta name="HandheldFriendly" content="true">
+    <!--
     <meta name="viewport" content="width=1008">
+     -->
   <?php endif; ?>
   <meta http-equiv="cleartype" content="on">
 
